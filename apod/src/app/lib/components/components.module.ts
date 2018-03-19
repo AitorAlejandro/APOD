1951847d248f2bf19350678fdfab7a8d854d864d
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2018-03-19 20:01:15 +0100

    app-footer and app-nav

--- apod/src/app/lib/components/components.module.ts
+++ apod/src/app/lib/components/components.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NavComponent]
+  declarations: [
+    NavComponent,
+    FooterComponent
+  ],
+  exports: [
+    NavComponent,
+    FooterComponent
+  ]
 })
 export class ComponentsModule { }
