97e575bf1c3fb6c48e1a0e6bdff9199586f7e9b5
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2018-03-21 22:01:53 +0100

    routing

--- apod/src/app/app.module.ts
+++ apod/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ComponentsModule } from './lib/components/components.module';
+import { AppRoutingModule } from './app.routing';
 import { HomeModule } from './views/home/home.module';
 
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     ComponentsModule,
     HomeModule
   ],
